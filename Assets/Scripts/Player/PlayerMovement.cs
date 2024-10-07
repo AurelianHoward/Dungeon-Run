aa1d8f24abdba5d47bef84e7fc7106e511ce827f
TeaSerpent <digitalShadow2523@gmail.com>
2024-10-07 22:22:59 +0100

    movementv2
    Movement updated and refined.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Lumin;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 movedirection;
     [SerializeField] float MoveSpeed = 10;
     private Rigidbody2D MyRigidBody;
-
+    [SerializeField] private SpriteRenderer SR;
+    [SerializeField] float Jumpforce = 1000;
     private void Awake()
     {
         MyRigidBody = GetComponent<Rigidbody2D>();
@@ -18,16 +21,44 @@ public class PlayerMovement : MonoBehaviour
         {
             movedirection = value.Get<Vector2>();
         }
-        
+
+    }
+
+    private void Onjump(InputValue value)
+    {
+        if (value !=null)
+        {
+            Jumpforce = value.Get<float>();
+        }
     }
 
     void Walk()
     {
-        MyRigidBody.linearVelocity = movedirection * MoveSpeed * Time.deltaTime;
+        //MyRigidBody.linearVelocity = movedirection * MoveSpeed * Time.deltaTime;
+        MyRigidBody.linearVelocityX = movedirection.x * MoveSpeed * Time.deltaTime;
+
+        if (MyRigidBody.linearVelocityX < 0)
+        {
+            SR.flipX = false;
+        }
+        if (MyRigidBody.linearVelocityX > 0)
+        {
+            SR.flipX = true;
+        }
+    }
+
+    void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump") && MyRigidBody.linearVelocityY == 0 )
+        {
+            MyRigidBody.linearVelocityY = Jumpforce * Time.deltaTime;
+        }
     }
 
     private void Update()
     {
         Walk();
+        Jump();
     }
 }
